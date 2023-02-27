93f1c877361778422464b6d20d4b17ed7c9cb616
laurb1988 <laurb1988@gmail.com>
2023-02-27 16:39:44 -0500

    first commit

--- realtors_project/settings.py
+++ realtors_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'WillbeCreatedAtInstall'
+SECRET_KEY = 'NotSharedOnGithub'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -147,8 +147,8 @@ MESSAGE_TAGS = {
 }
 
 #Email Settings
-EMAIL_HOST = 'yoursmtphostname'
+EMAIL_HOST = 'smtphost'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yoursmtpuser'
-EMAIL_HOST_PASSWORD = 'yourpass'
+EMAIL_HOST_USER = 'smtpuser'
+EMAIL_HOST_PASSWORD = 'smtppass'
 EMAIL_USE_TLS = True
